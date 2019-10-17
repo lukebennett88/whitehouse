269a11b617724754b3e1302b00f7122746d8d284
Luke Bennett <hello@lukebennett.com.au>
2019-10-17 14:35:11 +1100

    Style home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
-import Layout from '../components/layout';
+import Markdown from 'markdown-to-jsx';
 
-import lighthouse from '../static/www.phirannodesigns.com.au.json';
+import Layout from '../components/layout';
+import lighthouse from '../data/www.phirannodesigns.com.au.json';
 
 function Home() {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center">
+        <h2 className="font-black leading-none text-6xl text-center">
+          <span className="block">Report for: </span>
+          <span className="block leading-loose text-2xl">
+            {lighthouse.finalUrl}
+          </span>
+        </h2>
+        {/* {lighthouse.audits['redirects-http'].score === 0 && ( */}
         <dl>
-          <dt>URL</dt>
-          <dd>{lighthouse.finalUrl}</dd>
+          <dt>{lighthouse.audits['redirects-http'].title}</dt>
+          <dd>
+            <Markdown>
+              {lighthouse.audits['redirects-http'].description}
+            </Markdown>
+          </dd>
         </dl>
+        {/* )} */}
+        {/* {lighthouse.audits['viewport'].score === 0 && ( */}
+        <dl>
+          <dt>{lighthouse.audits.viewport.title}</dt>
+          <dd>
+            <Markdown>{lighthouse.audits.viewport.description}</Markdown>
+          </dd>
+        </dl>
+        {/* )} */}
       </div>
     </Layout>
   );
