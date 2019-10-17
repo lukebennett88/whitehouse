e5502a9bbe945f9c583b2646687822cb75b8be47
Luke Bennett <hello@lukebennett.com.au>
2019-10-17 14:30:58 +1100

    Remove footer

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 
 import Header from './header';
-import Footer from './footer';
 import '../css/tailwind.css';
 
 function Layout({ children }) {
@@ -11,7 +10,6 @@ function Layout({ children }) {
       <main className="flex-1 max-w-4xl mx-auto p-4 md:px-8 md:py-16 w-full">
         {children}
       </main>
-      <Footer />
     </div>
   );
 }
