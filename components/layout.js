116a05bf45c6201a9005a3ffeebff172765e0bc5
Luke Bennett <hello@lukebennett.com.au>
2019-10-22 15:44:21 +1100

    Remove Header from layout

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types';
 
-import Header from './header';
 import '../css/tailwind.css';
 
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-1 max-w-4xl mx-auto p-4 md:px-8 md:py-16 w-full">
         {children}
       </main>
