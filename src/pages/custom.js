d931876f5a8b0eab104c504a7fc19c5cdce1b30f
Luke Bennett <hello@lukebennett.com.au>
2019-11-05 13:43:38 +1100

    Add buttons to switch reports

--- src/pages/custom.js
+++ src/pages/custom.js
@@ -4,32 +4,51 @@ import Layout from '../components/layout';
 import Meta from '../components/meta';
 import Performance from '../components/custom-audits/performance';
 import Accessibility from '../components/custom-audits/accessibility';
-import BestPractices from '../components/best-practices';
+import BestPractices from '../components/custom-audits/best-practices';
 import SEO from '../components/seo';
 
-import lighthouse from '../data/eastlandtruss.com.au.json';
-// import lighthouse from '../data/phirannodesigns.com.au.json';
+import eastlandtruss from '../data/eastlandtruss.com.au.json';
+import phirannodesigns from '../data/phirannodesigns.com.au.json';
 
-const CustomAuditPage = () => (
-  <Layout>
-    <Meta title="Home" />
-    <h1 className="font-black leading-none mt-12 overflow-x-auto text-4xl w-full">
-      <span>Report for: </span>
-      <br />
-      <a
-        href={lighthouse.finalUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="leading-loose text-2xl text-indigo-700"
-      >
-        {lighthouse.finalUrl}
-      </a>
-    </h1>
-    {/* <Performance lighthouse={lighthouse} /> */}
-    <Accessibility lighthouse={lighthouse} />
-    <BestPractices lighthouse={lighthouse} />
-    {/* <SEO lighthouse={lighthouse} /> */}
-  </Layout>
-);
+const CustomAuditPage = () => {
+  const [lighthouse, setLighthouse] = React.useState(eastlandtruss);
+  return (
+    <Layout>
+      <Meta title="Home" />
+      <div className="-mx-2">
+        <button
+          onClick={() => setLighthouse(phirannodesigns)}
+          type="button"
+          className="bg-indigo-100 font-black px-4 py-1 mx-2 rounded-full text-indigo-800 text-xs tracking-wide uppercase hover:shadow"
+        >
+          Phiranno Designs
+        </button>
+        <button
+          onClick={() => setLighthouse(eastlandtruss)}
+          type="button"
+          className="bg-indigo-100 font-black px-4 py-1 mx-2 rounded-full text-indigo-800 text-xs tracking-wide uppercase hover:shadow"
+        >
+          Eastland Truss
+        </button>
+      </div>
+      <h1 className="font-black leading-none mt-12 overflow-x-auto text-4xl w-full">
+        <span>Report for: </span>
+        <br />
+        <a
+          href={lighthouse.finalUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="leading-loose text-2xl text-indigo-700"
+        >
+          {lighthouse.finalUrl}
+        </a>
+      </h1>
+      <Performance lighthouse={lighthouse} />
+      <Accessibility lighthouse={lighthouse} />
+      <BestPractices lighthouse={lighthouse} />
+      <SEO lighthouse={lighthouse} />
+    </Layout>
+  );
+};
 
 export default CustomAuditPage;
