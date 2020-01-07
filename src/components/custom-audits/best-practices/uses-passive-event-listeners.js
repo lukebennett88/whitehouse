89224b5116821579f1d25717dea1fb530edab1c3
Luke Bennett <hello@lukebennett.com.au>
2020-01-08 10:35:39 +1100

    Fix broken test

--- src/components/custom-audits/best-practices/uses-passive-event-listeners.js
+++ src/components/custom-audits/best-practices/uses-passive-event-listeners.js
@@ -2,43 +2,43 @@ import React from 'react';
 import Markdown from 'markdown-to-jsx';
 
 export const UsesPassiveEventListeners = ({ lighthouse }) =>
-  lighthouse.audits['no-vulnerable-libraries'].score !== 1 &&
-  lighthouse.audits['no-vulnerable-libraries'].score !== null && (
-    <li id="no-vulnerable-libraries">
+  lighthouse.audits['uses-passive-event-listeners'].score !== 1 &&
+  lighthouse.audits['uses-passive-event-listeners'].score !== null && (
+    <li id="uses-passive-event-listeners">
       <dl>
         <dt>
           <Markdown>
-            {lighthouse.audits['no-vulnerable-libraries'].title}
+            {lighthouse.audits['uses-passive-event-listeners'].title}
           </Markdown>
           <span
             className={`${
-              lighthouse.audits['no-vulnerable-libraries'].score < 0.5
+              lighthouse.audits['uses-passive-event-listeners'].score < 0.5
                 ? `bg-red-300 text-red-900`
                 : `bg-orange-300 text-orange-900`
             } font-black inline-block ml-2 px-2 rounded-full text-sm whitespace-no-wrap`}
           >
-            {lighthouse.audits['no-vulnerable-libraries'].displayValue}
+            {lighthouse.audits['uses-passive-event-listeners'].displayValue}
           </span>
         </dt>
         <dd>
           <details open>
             <summary>
               <Markdown>
-                {lighthouse.audits['no-vulnerable-libraries'].description}
+                {lighthouse.audits['uses-passive-event-listeners'].description}
               </Markdown>
             </summary>
             <div className="mt-4">
-              {lighthouse.audits['no-vulnerable-libraries'].details && (
+              {lighthouse.audits['uses-passive-event-listeners'].details && (
                 <div className="overflow-x-auto">
-                  <table className="mt-2 w-full">
+                  <table className="w-full mt-2">
                     <thead>
                       <tr>
                         {lighthouse.audits[
-                          'no-vulnerable-libraries'
+                          'uses-passive-event-listeners'
                         ].details.headings.map(heading => (
                           <th
                             key={heading.text}
-                            className="font-black px-4 py-2"
+                            className="px-4 py-2 font-black"
                           >
                             {heading.text}
                           </th>
@@ -46,18 +46,19 @@ export const UsesPassiveEventListeners = ({ lighthouse }) =>
                       </tr>
                     </thead>
                     <tbody>
-                      {lighthouse.audits['no-vulnerable-libraries'].details &&
+                      {lighthouse.audits['uses-passive-event-listeners']
+                        .details &&
                         lighthouse.audits[
-                          'no-vulnerable-libraries'
+                          'uses-passive-event-listeners'
                         ].details.items.map((item, index) => (
                           <tr key={index} className="odd:bg-white">
-                            <td title={item.url}>
-                              <div className="flex items-center py-2 px-4">
+                            <td>
+                              <div className="flex items-center px-4 py-2">
                                 {item.url}
                               </div>
                             </td>
-                            <td title={item.selector}>
-                              <div className="flex items-center py-2 px-4">
+                            <td>
+                              <div className="flex items-center px-4 py-2">
                                 {item.label}
                               </div>
                             </td>
